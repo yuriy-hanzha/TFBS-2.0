8e7aa71f6eb5aa9abc7b6211bf069f96cd6825aa
yuriy-hanzha <yurij.ganzha@gmail.com>
2016-05-17 22:08:45 +0300

    Added css to emails
    Emails are generated by using RazorEngine + PreMailer.Net (for inlining
    css to generated email by RazorEngine). Created templates of welcome and
    confirmation letters.

--- TheFeedbackSystem.API/MailService.cs
+++ TheFeedbackSystem.API/MailService.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Mail;
 using System.Web;
 using Microsoft.AspNet.Identity;
+using PreMailer.Net;
+using RazorEngine.Templating;
 using TheFeedbackSystem.API.Models;
 
 namespace TheFeedbackSystem.API
@@ -14,33 +17,33 @@ namespace TheFeedbackSystem.API
         private FeedBackDbContext _ctx;
         private UserManager<IdentityUser> _userManager;
         private SmtpClient smtpServer;
-        
+
         public MailService()
         {
             _ctx = new FeedBackDbContext();
             _userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(_ctx));
-            smtpServer = new SmtpClient(" ")
+            smtpServer = new SmtpClient("server.address")
             {
                 Port = 587,
-                Credentials = new System.Net.NetworkCredential(" ", " "),
+                Credentials = new System.Net.NetworkCredential("server.email@mail.net", "password"),
                 EnableSsl = false
             };
         }
 
         public void SendWelcomeMessage(IdentityUser user)
         {
+            var templateFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "EmailTemplates", "WelcomeEmail.cshtml");
+            var templateService = new TemplateService();
+            var emailHtmlBody = templateService.Parse(
+                File.ReadAllText(templateFilePath), new MailingModel() { User = user }, null, "WelcomeEmail");
+            var inlinedHtmlBody = PreMailer.Net.PreMailer.MoveCssInline(emailHtmlBody).Html;
+
             MailMessage mail = new MailMessage()
             {
                 From = new MailAddress("thefeedbacksystem@radacode.net"),
                 To = { user.Email },
                 Subject = "Welcome | TFBS",
-                Body = $"Hello, {user.UserName} <br/>" +
-                       $"Your account has been created - now you can send feedback about our service. <br/>" +
-                       $"<a href=\"http://localhost:27680/#/\">" +
-                       $"   <button>Get Started</button>" +
-                       $"</a><br/>" +
-                       $"Thank you for joining us! <br/>" +
-                       $"TFBS team",
+                Body = inlinedHtmlBody,
                 IsBodyHtml = true,
             };
 
@@ -58,20 +61,18 @@ namespace TheFeedbackSystem.API
             });
             _ctx.SaveChanges();
 
+            var templateFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "EmailTemplates", "ConfirmationEmail.cshtml");
+            var templateService = new TemplateService();
+            var emailHtmlBody = templateService.Parse(
+                File.ReadAllText(templateFilePath), new MailingModel() { User = user, ConfirmationKey = key }, null, "ConfirmationEmail");
+            var inlinedHtmlBody = PreMailer.Net.PreMailer.MoveCssInline(emailHtmlBody).Html;
+
             MailMessage mail = new MailMessage()
             {
                 From = new MailAddress("thefeedbacksystem@radacode.net"),
                 To = { user.Email },
                 Subject = "The last stage of registration | TFBS",
-                Body = $"<h3>Do not forget to confirm your TFBS account</h3>" +
-                       $"<h4>Hello, {user.UserName}! </br>" +
-                       $"You recently created an account on TFBS. Confirm it in order to complete the registration. <br/>" +
-                       $"<a href=\"http://localhost:27680/#/confirmation/{key}\">" +
-                       $"   <button>Verify your account</button>" +
-                       $"</a><br/>" +
-                       $"Thank you for joining us! <br/>" +
-                       $"TFBS team <br/>" +
-                       $"</h4>",
+                Body = inlinedHtmlBody,
                 IsBodyHtml = true,
             };
 
---
+++ TheFeedbackSystem.API/Models/MailingModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace TheFeedbackSystem.API.Models
+{
+    public class MailingModel
+    {
+        public IdentityUser User { get; set; }
+        public string ConfirmationKey { get; set; }
+    }
+}
